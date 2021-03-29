081f7819b06cd7d3dce2680d97c065231015879a
kar-21 <karthiks210198@gmail.com>
2021-03-29 20:17:17 +0530

    chaning backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,2 +1,2 @@
-export const environment = { production: true, backendAPI: 'https://ex-airline-mgmt-loc-prod.herokuapp.com' };
-// export const environment = { production: true, backendAPI: 'https://ex-airline-mgmt-prod.herokuapp.com' };
+// export const environment = { production: true, backendAPI: 'https://ex-airline-mgmt-loc-prod.herokuapp.com' };
+export const environment = { production: true, backendAPI: 'https://ex-airline-mgmt-prod.herokuapp.com' };
